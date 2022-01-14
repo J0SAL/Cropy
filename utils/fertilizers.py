4ca57b8977bef930a575ddf066b2904c186bd53d
Joy Almeida <52382282+J0SAL@users.noreply.github.com>
2022-01-15 00:11:55 +0530

    Add files via upload

---
+++ utils/fertilizers.py
@@ -0,0 +1,81 @@
+fertilizer_dic = {
+        'NHigh': 
+                """
+                <center><p>
+                The Nitrogen content of soil is <b>High!</b><br/>
+                <i>Please consider the following suggestions:</i>
+                </p></center>
+                <ol>
+                        <li><b>Add sawdust or fine woodchips to your soil: </b>The carbon in the sawdust/woodchips love nitrogen and will help absorb and soak up and excess nitrogen.</li>
+                        <li><b>Water your soil: </b>Soaking your soil with water will help leach the nitrogen deeper into your soil, effectively leaving less for your plants to use.</li>
+                        <li><b>Add Sugar: </b>In limited studies, it was shown that adding sugar to your soil can help potentially reduce the amount of nitrogen is your soil. Sugar is partially composed of carbon, an element which attracts and soaks up the nitrogen in the soil. This is similar concept to adding sawdust/woodchips which are high in carbon content.</li>
+                        <li><b>Plant heavy nitrogen feeding plants :</b>Tomatoes, corn, broccoli, cabbage and spinach are examples of plants that thrive off nitrogen and will suck the nitrogen dry.</li>
+                </ol>
+                """,
+
+        'Nlow': 
+                """
+                <center><p>
+                The Nitrogen content of soil is <b>Low!</b><br/>
+                <i>Please consider the following suggestions:</i>
+                </p></center>
+                <ol>
+                        <li><b>Add Composted Manure: </b>The most commonly used for fertilizer is cow or poultry manure, but horse, goat, sheep, and rabbit manure also work very well. Just make sure to compost the manure first or it can burn your plants.</li>
+                        <li><b>Crab meal: </b>Along with the 6% nitrogen levels, crab meal contains a high portion of lime, so you can use it if you need to make your soil more alkaline. Avoid if you have soils that already have a high pH.</li>
+                        <li><b>Rich Compost: </b>The holy grail of garden amendments—it does it all! While compost has a low level of available nitrogen, it does work well as a source of slow-release nitrogen—especially when combined with fast-acting nitrogen fertilizers like fish emulsion.</li>
+                        <li><b>Corn Gluten: </b>Corn gluten works as both a nitrogen-rich soil amendment and a weed preventer. However, it keeps all plants from setting roots—so only use this around established plants.</li>
+                </ol>
+                """,
+
+        'PHigh': 
+                """
+                <center><p>
+                The Phosphorus content of soil is <b>High!</b><br/>
+                <i>Please consider the following suggestions:</i>
+                </p></center>
+                <ol>
+                        <li><b>Avoid adding manure as fertilizer: </b>Manure is typically high in phosphorous and can quickly lead to a spike in phosphorous levels.</li>
+                        <li><b>Nitrogen-fixing vegetables: </b>Plant nitrogen-fixing vegetables to increase nitrogen without increasing phosphorous.  Examples of such vegetables include beans and peas. If nitrogen levels are extremely low, planting nitrogen-fixing vegetables alone will not fix your problem.</li>
+                </ol>
+                """,
+
+        'Plow': 
+                """
+                <center><p>
+                The Phosphorus content of soil is <b>Low!</b><br/>
+                <i>Please consider the following suggestions:</i>
+                </p></center>
+                <ol>
+                        <li><b>Bone meal: </b>a fast acting source that is made from ground animal bones which is rich in phosphorous.</li>
+                        <li><b>Organic compost: </b>adding quality organic compost to your soil will help increase phosphoos content.</li>
+                        <li><b>Manure: </b>as with compost, manure can be an excellent source of phosphorous for your plants.</li>
+                        <li><b>Ensure proper soil pH: </b>having a pH in the 6.0 to 7.0 range has been scientifically proven to have the optimal phosphorus uptake in plants.</li>
+                </ol>
+                """,
+
+        'KHigh': 
+                """
+                <center><p>
+                The Potassium content of soil is <b>High!</b><br/>
+                <i>Please consider the following suggestions:</i>
+                </p></center>
+                <ol>
+                        <li><b>Sifting the soil: </b>Sift the soil and remove as many rocks as possible. This will prevent minerals in rocks, such as feldspar and mica, from releasing potassium into the soil.</li>                        
+                        <li><b>Loosen the Soil: </b>Loosen the soil with a garden fork or shovel, then water deeply to dissolve and flush out the surplus in potassium-rich soil. Allow the soil to dry completely, then repeat two or three more times.</li>
+                        <li><b>Digging : </b>If the area is small, digging in crushed seashells or eggshells may help balance soil nutrients.</li>
+                </ol>
+                """,
+
+        'Klow': 
+                """
+                <center><p>
+                The Potassium content of soil is <b>Low!</b><br/>
+                <i>Please consider the following suggestions:</i>
+                </p></center>
+                <ol>
+                        <li><b>Add Kelp Or Seaweed To Your Soil: </b>If you live near the ocean or have access to a natural source of seaweed, you can add this to your soil to raise its potassium level. For garden beds, you can use natural seaweed as a mulch as it will break down over time and nourish your soil.</li>
+                        <li><b>Using Wood Ash: </b>If you have an open fire place or a wood heater, don’t throw the ash into the trash. The ashes from your fire are high in potassium and can be spread thinly over your soil or lawn area. Take note though, that the wood ash will raise your soil’s pH level.</li>
+                        <li><b>Add Compost To Your Soil: </b>Compost made from lots of vegetable scraps and green material along with some wood ash is ideal for adding nutrients to your soil including potassium.</li>
+                </ol>
+                """
+    }
