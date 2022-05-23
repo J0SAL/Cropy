d0dcaa2a70d94429f71a18d842f35ae9b97f16fb
Josal <joyalmeida321@gmail.com>
2022-05-24 01:44:31 +0530

    added DL model for disease prediction

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ import requests
 import config
 import pickle
 
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
 from utils.fertilizers import fertilizer_dic
+from utils.diseases import disease_dic
 
 # -------------------------LOADING THE TRAINED MODELS -------------------------
 
@@ -17,6 +22,9 @@ from utils.fertilizers import fertilizer_dic
 
 model_path = 'models/randomf.pkl'
 model = pickle.load(open(model_path,'rb'))
+
+
+MODEL = tf.keras.models.load_model("models/leaf_disease_1")
 
 
 # Loading Weather data from OpenWeatherAPI
@@ -58,11 +66,6 @@ def home():
     title = 'Cropy - Home'
     return render_template('index.html', title=title)
 
-#render try again
-@ app.route('/na')
-def tryagain():
-    title = 'page not found'
-    return render_template('try-again.html', title=title)
 
 # render crop recommendation form page
 
@@ -79,6 +82,11 @@ def fertilizer_recommendation():
 
     return render_template('fertilizer.html', title=title)
 
+@ app.route('/joy')
+def joy():
+    title = 'joy'
+
+    return render_template('disease-result.html', title=title)
 
 
 # ===============================================================================================
@@ -116,7 +124,7 @@ def crop_prediction():
 
 @ app.route('/fertilizer-predict', methods=['POST'])
 def fert_recommend():
-    title = 'Harvestify - Fertilizer Suggestion'
+    title = 'Cropy - Fertilizer Suggestion'
 
     crop_name = str(request.form['cropname'])
     N = int(request.form['nitrogen'])
@@ -124,7 +132,7 @@ def fert_recommend():
     K = int(request.form['pottasium'])
     # ph = float(request.form['ph'])
 
-    df = pd.read_csv('Data/my_fertilizers.csv')
+    df = pd.read_csv('data/my_fertilizers.csv')
 
     nr = df[df['Crop'] == crop_name]['N'].iloc[0]
     pr = df[df['Crop'] == crop_name]['P'].iloc[0]
@@ -156,6 +164,87 @@ def fert_recommend():
     return render_template('fertilizer-result.html', recommendation=response, title=title)
 
 
+# render disease result page
+
+# disease names
+CLASS_NAMES = [
+'Apple___Apple_scab',
+ 'Apple___Black_rot',
+ 'Apple___Cedar_apple_rust',
+ 'Apple___healthy',
+ 'Blueberry___healthy',
+ 'Cherry_(including_sour)___Powdery_mildew',
+ 'Cherry_(including_sour)___healthy',
+ 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+ 'Corn_(maize)___Common_rust_',
+ 'Corn_(maize)___Northern_Leaf_Blight',
+ 'Corn_(maize)___healthy',
+ 'Grape___Black_rot',
+ 'Grape___Esca_(Black_Measles)',
+ 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+ 'Grape___healthy',
+ 'Orange___Haunglongbing_(Citrus_greening)',
+ 'Peach___Bacterial_spot',
+ 'Peach___healthy',
+ 'Pepper,_bell___Bacterial_spot',
+ 'Pepper,_bell___healthy',
+ 'Potato___Early_blight',
+ 'Potato___Late_blight',
+ 'Potato___healthy',
+ 'Raspberry___healthy',
+ 'Soybean___healthy',
+ 'Squash___Powdery_mildew',
+ 'Strawberry___Leaf_scorch',
+ 'Strawberry___healthy',
+ 'Tomato___Bacterial_spot',
+ 'Tomato___Early_blight',
+ 'Tomato___Late_blight',
+ 'Tomato___Leaf_Mold',
+ 'Tomato___Septoria_leaf_spot',
+ 'Tomato___Spider_mites Two-spotted_spider_mite',
+ 'Tomato___Target_Spot',
+ 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+ 'Tomato___Tomato_mosaic_virus',
+ 'Tomato___healthy']
+
+
+def read_file_as_image(data):
+    image = np.array(Image.open(BytesIO(data)))
+    #np array <- pillow image <- binary stream <- bytes
+    return image
+
+
+@app.route('/disease-predict', methods=['GET', 'POST'])
+def disease_prediction():
+    title = 'Cropy - Disease Detection'
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files.get('file')
+        if not file:
+            return render_template('disease.html', title=title)
+        try:
+            file = file.read() #byte data
+            image = read_file_as_image(file)
+
+            image = tf.image.resize(image, [256,256]).numpy()
+
+            image = np.expand_dims(image, 0) # 1d to 2d
+            predictions = MODEL.predict(image)
+
+            predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+            confidence = round(np.max(predictions[0])*100,2) # 0 because provided only one image
+
+            data = {
+                'confidence': float(confidence),
+                'data': Markup(str(disease_dic[predicted_class]))
+            }
+            return render_template('disease-result.html', prediction=data, title=title)
+        except:
+            pass
+    return render_template('disease.html', title=title)
+
 # ===============================================================================================
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False);
